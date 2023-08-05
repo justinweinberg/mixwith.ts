4b032afe3da2340216de34e39a5277bc62a1af66
Justin Weinberg <justn@modrg.com>
2023-08-05 17:15:40 -0500

    cleanup

--- mod.ts
+++ mod.ts
@@ -2,16 +2,3 @@ import {mix} from './mixwith.ts'
 import type { Constructable } from './types.d.ts';
 export {mix} 
 export type {Constructable};
-/**
- * @class foo
- * simple class
- */
-class foo {
-
-    /**
-     * simple member
-     */
-    m()  {
-        return "hello"
-    }
-}
